7eae0a6c7a9e581c596eceaeca3663a8e32a58f8
yourpis <dafizzafrz@gmail.com>
2026-06-21 21:17:30 +0700

    Fix Prisma config type error

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  engine: "classic",
   datasource: {
     url: env("DATABASE_URL"),
   },
